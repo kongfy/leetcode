f52a503341cf1ee4ff6ec51bfa5d3fe228f63d22
Fanyu Kong <njukongfy@gmail.com>
2014-12-28 15:28:11 +0800

    improve longest substring

--- longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters.cpp
@@ -5,47 +5,28 @@ using namespace std;
 
 class Solution
 {
-    int _table[256];
-
-    void mark(char c, int index)
-    {
-        _table[c] = index;
-    }
-
-    int check(char c)
-    {
-        return _table[c];
-    }
-
-    void clear()
-    {
-        for (int i = 0; i < 256; ++i) {
-            _table[i] = -1;
-        }
-    }
 public:
     int lengthOfLongestSubstring(string s)
     {
         int ans = 0;
         int start = 0, len = 0;
 
-        clear();
+        int *table = new int[256];
+        for (int i = 0; i < 256; ++i) {
+            table[i] = -1;
+        }
+
         for (int i = 0; i < s.size(); ++i) {
             char c = s[i];
+            int index = table[c];
 
-            int index = check(c);
-            if (index >= 0) {
+            if (index >= 0 && index >= start) {
                 ans = max(ans, len);
-
-                for (int j = start; j <= index; ++j) {
-                    mark(s[j], -1);
-                }
-
                 len -= index - start + 1;
                 start = index + 1;
             }
 
-            mark(c, i);
+            table[c] = i;
             len ++;
         }
 
@@ -58,9 +39,11 @@ int main(int argc, char *argv[])
 {
     Solution solve;
 
+    string s0("abcabcbb");
     string s1("wlrbbmqbhcdarzowkkyhiddqscdxrjmowfrxsjybldbefsarcbynecdyggxxpklorellnmpapqfwkhopkmco");
     string s2("hchzvfrkmlnozjk");
     string s3("qopubjguxhxdipfzwswybgfylqvjzhar");
+    cout << solve.lengthOfLongestSubstring(s0) << endl;
     cout << solve.lengthOfLongestSubstring(s1) << endl;
     cout << solve.lengthOfLongestSubstring(s2) << endl;
     cout << solve.lengthOfLongestSubstring(s3) << endl;
